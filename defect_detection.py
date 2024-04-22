f4d48148169bef6e504b55b27998500671ef103f
Disha Shah <disha9896@gmail.com>
2024-04-22 00:04:24 -0400

    Initial commit transferred data from RIT's Gitlab

---
+++ defect_detection.py
@@ -0,0 +1,193 @@
+import os
+import shutil
+from pathlib import Path
+import yaml
+from ultralytics import YOLO
+
+class ObjectDetection:
+    def __init__(self, model_name="yolov9n.pt", data_dir="data", test_dir="test_outputs"):
+        self.model_name = model_name
+        self.data_dir = data_dir
+        self.test_dir = test_dir
+        self.data_config = None
+        self.model = None
+        self.classes = None
+
+    def data_preprocessing(self):
+        """Resize images and convert then to gray scale 
+        """
+        pass
+
+    def load_dataset(self, dataset_path):
+        """Load dataset from directory 
+
+        Args:
+            dataset_path : path to the dataset containing images, labels and yaml 
+
+        Raises:
+            ValueError: If the dataset URL is invalid or the download fails.
+        """
+        try:
+            self.data_config = self.validate_dataset(dataset_path)
+        except Exception as e:
+            raise ValueError(f"Failed to load dataset: {e}")
+
+
+
+    def validate_dataset(self, data_dir):
+        """
+        Validates the dataset format for YOLOv9 based on the specified structure.
+
+        Args:
+            data_dir (str): Path to the root directory of the dataset.
+            data_config_path (str): Path to the dataset configuration YAML file.
+
+        Raises:
+            ValueError: If any validation check fails.
+        """
+        # check all the required folder are present in the folder
+        required_folders = ['train', 'valid', 'test']
+        data_config_path = Path(data_dir) / 'data.yaml'  # YAML file name is 'data.yaml'
+
+        # Check if all required folders exist
+        for folder in required_folders:
+            if not os.path.exists(Path(data_dir) / folder):
+                raise ValueError(f"Required folder '{folder}' not found in dataset directory.")
+
+        # Check if YAML file exists
+        if not os.path.exists(data_config_path):
+            raise ValueError(f"Dataset configuration file 'data.yaml' not found.")
+
+        # Check YAML format and required keys
+        with open(data_config_path, 'r') as f:
+            try:
+                data_config = yaml.safe_load(f)
+            except yaml.YAMLError as e:
+                raise ValueError(f"Invalid YAML format: {e}")
+
+        required_keys = ['train', 'val', 'test', 'nc', 'names']
+        unexpected_keys = set(data_config.keys()) - set(required_keys)
+
+
+        if unexpected_keys:
+            raise ValueError(f"Unexpected keys found: {unexpected_keys}")
+        
+        missing_keys = set(required_keys) - set(data_config.keys())
+        if missing_keys:
+            raise ValueError(f"Missing required keys in YAML file: {', '.join(missing_keys)}")
+
+        if not isinstance(data_config['nc'], int):
+            raise ValueError(f"nc key must be an integer (number of classes).")
+
+        # Validate image and label folder existence for train, val, test splits
+       
+        for split in ['train', 'valid', 'test']:
+            image_dir = Path(data_dir) / split / 'images'
+            label_dir = Path(data_dir) / split / 'labels'
+            if not os.path.exists(image_dir):
+                raise ValueError(f"Image directory not found: {image_dir}")
+
+            if not os.path.exists(label_dir):
+                raise ValueError(f"Label directory not found: {label_dir}")
+
+            # Check for .txt files in label directory
+            label_files = os.listdir(label_dir)
+            if not all(file.endswith('.txt') for file in label_files):
+                raise ValueError(f"Label directory ({label_dir}) must contain only .txt files.")
+        
+        return data_config_path
+    
+
+
+    def load_model(self, pretrained_model = "Yolov9_Training/yolov9e.pt"):
+        """Loads the YOLOv9 model."""
+        self.model = YOLO(pretrained_model)
+        
+
+
+    def train(self, epochs=1, batch=-1, imgsz= 640):
+        """Train the yolo model on pre trained yolov9e model. 
+        After training this function will store following components in the run/detect/train folder
+        - weights 
+            - Last.pt (latest trained model)
+            - best.pt (best out of the trained model)
+        - Different model validation graphs.
+        - Different performance metrics in the excel file.
+        - sample detected images.
+        
+        Args:
+            epochs (int) : Number of epochs you want to use to train the model
+            batch (int) : specify the batch size to train the model
+            imgsz (int) : size of the image
+            save_dir (str) : save the logs of the file at that path
+        """
+        self.model.train(data = self.data_config, epochs=epochs, batch=batch, imgsz=imgsz)
+    
+
+    def restart_training(self, saved_weights = 'runs/detect/train7/weights/last.pt'):
+        """Start training the model from where you left at last. 
+
+        Args:
+            saved_weights - After training the model it will store the last state of
+            the trained model in runs/detect/train/weights/last.pt This will be stored
+            in the working directory.
+        
+        """
+        self.model = YOLO(saved_weights)  # load a partially trained model
+
+        # Resume training
+        self.model.train(resume=True)
+    
+    def model_prediction(self, model, image_path):
+        """Predict the values of the trained model. First load the model using load_model function
+        """
+        self.load_model(model)
+        # model = YOLO(model)  # load a custom model
+        # for file in os.listdir("/home/dssgis/ondemand/Yolov9_training/Dataset_split/test/images"):
+        results = self.model.predict(image_path)
+        for result in results:
+            boxes = result.boxes  # Boxes object for bounding box outputs
+            masks = result.masks  # Masks object for segmentation masks outputs
+            keypoints = result.keypoints  # Keypoints object for pose outputs
+            probs = result.probs  # Probs object for classification outputs
+            result.show()  # display to screen
+            # result.save(filename='/home/dssgis/Results/capture.jpg')  # save to disk
+    
+
+    def predict_multiple(self, model, folder_path, result_path="results"):
+        """Predict the images in the folder and store those images to results folder
+        """
+
+        self.load_model(model)
+        for file in os.listdir(folder_path):
+            extension = file.split(".")[-1]
+            if extension not in ["jpg", "jpeg", "png"]:
+                continue
+            results = self.model.predict(os.path.join(folder_path,file))
+            for result in results:
+                boxes = result.boxes  # Boxes object for bounding box outputs
+                masks = result.masks  # Masks object for segmentation masks outputs
+                keypoints = result.keypoints  # Keypoints object for pose outputs
+                probs = result.probs  # Probs object for classification outputs
+                # result.show()  # display to screen
+                result.save(filename=os.path.join("results",file))  # save to disk
+
+            
+
+if __name__ == "__main__":
+
+    # Example usage (replace with your actual paths)
+    data_dir = "Dataset"
+    data_config_path = "Dataset/data.yaml"
+    od = ObjectDetection()
+
+    try:
+        # od.load_dataset(data_dir)
+        # od.load_model("model/best.pt")
+        # od.train()
+        # od.model_prediction("model/best.pt", "Dataset/test/images/img_01_4402724300_00001_jpg.rf.7bcfacc21bccbec82f4e03e748484b35.jpg")
+        od.predict_multiple("model/best.pt", "Dataset/test/images")
+        
+    except ValueError as e:
+        print(f"Dataset format validation error: {e}")
+
---
+++ video_testing.py
@@ -0,0 +1,64 @@
+from defect_detection import ObjectDetection
+from ultralytics import YOLO
+import cv2
+import math 
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
+model = YOLO()
+
+# object classes
+classNames = ["crease", "crescent_gap", "d" ,"inclusion", "oil_spot","punching_hole",
+               "rolled_pit", "silk_spot","waist folding","water_spot","welding_line"]
+
+cam = cv2.VideoCapture(r"Z:\Disha\Defect_detection\videos\2024-04-09_10-36-32.mp4") 
+frameTime = 20
+success, img = cam.read()
+height, width, layers = img.shape
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter(r"Z:\Disha\Defect_detection\Results\videos\output2.avi", fourcc, 20.0, (width, height))
+
+while(cam.isOpened()):
+    success, img = cam.read()
+    results = model(img, stream=True)
+
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
+    video.write(img)
+    if cv2.waitKey(frameTime) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+video.release()
